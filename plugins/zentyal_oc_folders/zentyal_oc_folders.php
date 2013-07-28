ac180a01f3a6d9900ca492ad01634f06101875ab
Miguel Julián <mjulian@zentyal.com>
2013-07-28 18:51:34 +0200

    Roundcube folders structure
    Also updated .gitignore to omit *.swp

---
+++ plugins/zentyal_oc_folders/zentyal_oc_folders.php
@@ -0,0 +1,9 @@
+<?php
+    class zentyal_oc_folders extends rcube_plugin
+    {
+        function init(){
+            $this->rc = rcmail::get_instance();
+            echo "mumumuuuuuuuu";
+        }
+    }
+?>
