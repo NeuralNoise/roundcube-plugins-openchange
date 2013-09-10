313efad3de8322ccb840769e2cd2bae1d05382cb
Miguel Julián <mjulian@zentyal.com>
2013-09-11 00:21:48 +0200

    Several improvements at contacts parser class
     * Added all possible properties (but the photo)
     * Which properties are queried can be set into an array
     * Retrievent and recursive parsing of the properties
     * Some function renaming and refactoring

--- plugins/zentyal_oc_contacts/OcContactsParser.php
+++ plugins/zentyal_oc_contacts/OcContactsParser.php
@@ -1,8 +1,8 @@
 <?php
-    /**
-     * Aux functions begin
-     */
-
+/**
+ * Static class that will help to parse Openchange contacts data
+ * to the way Roundcube would understand it.
+ */
 class OcContactsParser
 {
     /**
@@ -16,67 +16,89 @@ class OcContactsParser
      * It seems that rcube maidenname is not pressent a MS-OXOCNTC
      * TODO: Add the photo management
      */
-    public static $contact_field_translation = array(
-            'id'                        => 'ID',
-            'PidTagDisplayName'         => 'name',
-            'PidTagNickname'            => 'nickname',
-            'PidTagGeneration'          => 'suffix',
-            'PidTagDisplayNamePrefix'   => 'prefix',
-            'PidTagGivenName'           => 'firstname',
-            'PidTagSurname'             => 'surname',
-            'PidTagMiddleName'          => 'middlename',
 
-            'PidTagTitle'           =>'jobtitle',
-            'PidTagDepartamentName' => 'department',
-            'PidTagCompanyName'     => 'organization',
+    public static $full_contact_properties = array(
+        PidTagDisplayName,PidTagNickname,PidTagGeneration,PidTagDisplayNamePrefix,
+        PidTagGivenName,PidTagSurname,PidTagMiddleName,PidTagTitle,
+        PidTagDepartmentName,PidTagCompanyName,PidTagGender,
+        PidTagAssistant,PidTagManagerName,PidTagSpouseName,PidTagBirthday,
+        PidTagWeddingAnniversary,PidLidEmail1EmailAddress,
+        PidLidEmail2EmailAddress,PidLidEmail3EmailAddress,
+        PidTagBusinessFaxNumber,PidTagHomeFaxNumber,PidTagPrimaryTelephoneNumber,
+        PidTagBusinessTelephoneNumber,PidTagBusiness2TelephoneNumber,
+        PidTagHomeTelephoneNumber,PidTagHome2TelephoneNumber,
+        PidTagMobileTelephoneNumber,PidTagCarTelephoneNumber,
+        PidTagAssistantTelephoneNumber,PidTagOtherTelephoneNumber,
+        PidTagHomeAddressStreet,PidLidWorkAddressStreet,PidTagOtherAddressStreet,
+        PidTagHomeAddressCity,PidLidWorkAddressCity,PidTagOtherAddressCity,
+        PidTagHomeAddressPostalCode,PidLidWorkAddressPostalCode,
+        PidTagOtherAddressPostalCode,PidTagHomeAddressStateOrProvince,
+        PidLidWorkAddressState,PidTagOtherAddressStateOrProvince,
+        PidTagHomeAddressCountry,PidLidWorkAddressCountry,
+        PidTagOtherAddressCountry,PidLidInstantMessagingAddress,
+        PidTagPersonalHomePage,PidTagBusinessHomePage, PidTagBody
+    );
+
+    public static $contact_field_translation = array(
+            PidTagDisplayName       => 'name',
+            PidTagNickname          => 'nickname',
+            PidTagGeneration        => 'suffix',
+            PidTagDisplayNamePrefix => 'prefix',
+            PidTagGivenName         => 'firstname',
+            PidTagSurname           => 'surname',
+            PidTagMiddleName        => 'middlename',
+
+            PidTagTitle             =>'jobtitle',
+            PidTagDepartmentName    => 'department',
+            PidTagCompanyName       => 'organization',
 
             /* 0 unespecified, 1 female, 2 male */
-            'PidTagGender'              => 'gender',
-            'PidTagAssistant'           => 'assistant',
-            'PidTagManagerName'         => 'manager',
-            'PidTagSpouseName'          => 'spouse',
-            'PidTagBirthday'            => 'birthday',
-            'PidTagWeddingAnniversary'  => 'anniversary',
+            PidTagGender                => 'gender',
+            PidTagAssistant             => 'assistant',
+            PidTagManagerName           => 'manager',
+            PidTagSpouseName            => 'spouse',
+            PidTagBirthday              => 'birthday',
+            PidTagWeddingAnniversary    => 'anniversary',
 
+            PidLidEmail1EmailAddress    => '@email:home',
+            PidLidEmail2EmailAddress    => '@email:work',
+            PidLidEmail3EmailAddress    => '@email:other',
 
-            'PidLidEmail1EmailAddress'  => '@email:home',
-            'PidLidEmail2EmailAddress'  => '@email:work',
-            'PidLidEmail3EmailAddress'  => '@email:other',
+            PidTagBusinessFaxNumber => '@phone:workfax',
+            PidTagHomeFaxNumber     => '@phone:homefax',
 
-            'PidTagPrimaryFaxNumber'    => '@phone:workfax',
-            'PidTagBusinessFaxNumber'   => '@phone:workfax',
-            'PidTagHomeFaxNumber'       => '@phone:homefax',
+            PidTagPrimaryTelephoneNumber    => '@phone:main',
+            PidTagBusinessTelephoneNumber   => '@phone:work',
+            PidTagBusiness2TelephoneNumber  => '@phone:work2',
+            PidTagHomeTelephoneNumber       => '@phone:home',
+            PidTagHome2TelephoneNumber      => '@phone:home2',
+            PidTagMobileTelephoneNumber     => '@phone:mobile',
+            PidTagCarTelephoneNumber        => '@phone:car',
+            PidTagAssistantTelephoneNumber  => '@phone:assistant',
+            PidTagOtherTelephoneNumber      => '@phone:other',
 
-            'PidTagPrimaryTelephoneNumber'      => '@phone:main',
-            'PidTagBusinessTelephoneNumber'     => '@phone:work',
-            'PidTagBusiness2TelephoneNumber'    => '@phone:work2',
-            'PidTagHomeTelephoneNumber'         => '@phone:home',
-            'PidTagHome2TelephoneNumber'        => '@phone:home2',
-            'PidTagMobileTelephoneNumber'       => '@phone:mobile',
-            'PidTagCarTelephoneNumber'          => '@phone:car',
-            'PidTagAssistantTelephoneNumber'    => '@phone:assistant',
-            'PidTagOtherTelephoneNumber'        => '@phone:other',
+            PidTagHomeAddressStreet           => 'address:home/street',
+            PidLidWorkAddressStreet           => 'address:work/street',
+            PidTagOtherAddressStreet          => 'address:other/street',
+            PidTagHomeAddressCity             => 'address:home/locality',
+            PidLidWorkAddressCity             => 'address:work/locality',
+            PidTagOtherAddressCity            => 'address:other/locality',
+            PidTagHomeAddressPostalCode       => 'address:home/zipcode',
+            PidLidWorkAddressPostalCode       => 'address:work/zipcode',
+            PidTagOtherAddressPostalCode      => 'address:other/zipcode',
+            PidTagHomeAddressStateOrProvince  => 'address:home/region',
+            PidLidWorkAddressState            => 'address:work/region',
+            PidTagOtherAddressStateOrProvince => 'address:other/region',
+            PidTagHomeAddressCountry          => 'address:home/country',
+            PidLidWorkAddressCountry          => 'address:work/country',
+            PidTagOtherAddressCountry         => 'address:other/country',
 
-            'PidTagHomeAddressStreet'           => 'address:home/street',
-            'PidTagWorkAddressStreet'           => 'address:work/street',
-            'PidTagOtherAddressStreet'          => 'address:other/street',
-            'PidTagHomeAddressCity'             => 'address:home/locality',
-            'PidTagWorkAddressCity'             => 'address:work/locality',
-            'PidTagOtherAddressCity'            => 'address:other/locality',
-            'PidTagHomeAddressPostalCode'       => 'address:home/zipcode',
-            'PidTagWorkAddressPostalCode'       => 'address:work/zipcode',
-            'PidTagOtherAddressPostalCode'      => 'address:other/zipcode',
-            'PidTagHomeAddressStateOrProvince'  => 'address:home/region',
-            'PidTagWorkAddressStateOrProvince'  => 'address:work/region',
-            'PidTagOtherAddressStateOrProvince' => 'address:other/region',
-            'PidTagHomeAddressCountry'          => 'address:home/country',
-            'PidTagWorkAddressCountry'          => 'address:work/country',
-            'PidTagOtherAddressCountry'         => 'address:other/country',
+            PidLidInstantMessagingAddress => '@im:other',
 
-            'PidTagInstantMessagingAddress'=> '@im:other',
+            PidTagPersonalHomePage  => '@website:homepage',
+            PidTagBusinessHomePage  => '@website:work',
 
-            'PidTagPersonalHomePage'    => '@website:homepage',
-            'PidTagBusinessHomePage'    => '@website:work',
+            PidTagBody => 'notes',
             );
 
     /**
@@ -121,37 +143,79 @@ $file = '/var/log/roundcube/my_debug.txt';
 $handle = fopen($file, 'a');
 fwrite($handle, "\nStarting getFullContact id = " . $id . "\n");
 
-        $resultContact = array();
+    /* TODO: manage de id -> ID conversion */
         $contactId = self::getContactId($id, "/", true);    
         $fetchedContact = $contacts->openMessage($contactId);
         $contactProperties = call_user_func_array(
-                array($fetchedContact, 'get'), $propertiesList);
+                array($fetchedContact, 'get'),
+                self::$full_contact_properties);
+
+        $resultContact = self::parseProperties($contactProperties);
+        $resultContact['ID'] = $id;
+
+ob_start(); var_dump($resultContact);
+fwrite($handle, "this is the contact:\n" . ob_get_clean() . "\n");
 
 fclose($handle);
         return $resultContact;
     }
 
-    private function parse_recursive_field($key, $value, $contact)
+    private static function parseProperties($properties)
+    {
+$file = '/var/log/roundcube/my_debug.txt';
+$handle = fopen($file, 'a');
+        $contact = array();
+
+        $i = 0;
+
+        foreach ($properties as $prop => $field) {
+            $prop = self::$full_contact_properties[$i];
+            $i++;
+fwrite($handle, "prop: " . $prop . " | field: " . $field . "\n");
+            $contact = self::parseProperty($prop, $field, $contact);    
+        }
+fclose($handle);
+        return $contact;
+    }
+
+    private static function parseProperty($key, $value, $contact)
     {
     /* TODO: if an array has elements, push into it, not replace */
+
+$file = '/var/log/roundcube/my_debug.txt';
+$handle = fopen($file, 'a');
+fwrite($handle, "===> ");
         $result = $contact;
 
+        $key = self::$contact_field_translation[$key];
         $keys = explode("/", $key);
+        
+        if ($value) {
+            if (self::valueHasToBeArray($key)) {
+                $finalValue = array($value);
+                $keys[0] = substr($keys[0], 1);
+            } else {
+                $finalValue = $value;
+            }
 
-        if (count($keys) == 1)
-            $result[$keys[0]] = $value;
-        else if (count($keys) == 2)
-            $result[$keys[0]][$keys[1]] = $value;
+    
+    fwrite($handle, "prop: " . serialize($keys) . " | field: " . $finalValue . "\n");
 
+            if (count($keys) == 1)
+                $result[$keys[0]] = $finalValue;
+            else if (count($keys) == 2)
+                $result[$keys[0]][$keys[1]] = $finalValue;
+        }
+fclose($handle);
         return $result;
     }
 
-    private function key_is_correct($key)
+    private static function keyIsCorrect($key)
     {
         return array_key_exists($key, $this->contact_field_translation);
     }
 
-    private function value_has_to_be_array($key)
+    private static function valueHasToBeArray($key)
     {
         return preg_match("/@/", $key);
     }
