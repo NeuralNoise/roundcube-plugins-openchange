fc83f97810af882dfb1878086ececd35e7ba88ca
José Antonio Calvo <jacalvo@zentyal.com>
2013-09-27 16:27:29 +0200

    fixed packaging

--- plugins/OpenchangeConfig.php
+++ plugins/OpenchangeConfig.php
@@ -1,12 +1,13 @@
 <?php
 class OpenchangeConfig
 {
+    //FIXME!
     public static $profileLocation = "/tmp/profiles.ldb";
 
     public static $logLocation = "/var/log/roundcube/roundcube-openchange";
     public static $debugEnabled = false;
 
-    public static $openchangeServerIP = "192.168.56.56";
-    public static $openchangeServerDomain= "example";
+    public static $openchangeServerIP = "localhost";
+    public static $openchangeServerDomain = "zentyal-domain.lan";
 }
 ?>
