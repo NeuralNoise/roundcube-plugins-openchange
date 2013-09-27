70e8277752e50af137359a3f68164f77b0897b73
Miguel Julián <mjulian@zentyal.com>
2013-09-27 16:05:09 +0200

    Turned debug into false for merging

--- plugins/OpenchangeConfig.php
+++ plugins/OpenchangeConfig.php
@@ -4,7 +4,7 @@ class OpenchangeConfig
     public static $profileLocation = "/tmp/profiles.ldb";
 
     public static $logLocation = "/var/log/roundcube/roundcube-openchange";
-    public static $debugEnabled = true;
+    public static $debugEnabled = false;
 
     public static $openchangeServerIP = "192.168.56.56";
     public static $openchangeServerDomain= "example";
