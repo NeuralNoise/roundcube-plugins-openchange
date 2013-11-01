accffd458ad1fc82ff593456c78563fc44156660
Miguel Julián <mjulian@zentyal.com>
2013-11-01 09:29:34 +0100

    Profile checking at login managed by MapiSessionHandler class
    So we have more control about the mapi objects destruction order

--- plugins/zentyal_lib/MapiSessionHandler.php
+++ plugins/zentyal_lib/MapiSessionHandler.php
@@ -15,11 +15,17 @@
 class MapiSessionHandler
 {
     private $debug;
+
+    private $sessionStarted = false;
+
     private $profileDB;
     private $mapiProfile;
     private $session;
     private $mailbox;
     private $folder;
+
+    private $mapiDB;
+    private $profile;
 
     // Used to get the PHP bindings function name to get the given folder
     private $bindingsFunctions = array(
@@ -57,6 +63,7 @@ class MapiSessionHandler
                 $this->debug->writeMessage("Mailbox opened", 0, "4");
                 $this->folder = $this->openMailboxFolder($folder);
                 $this->debug->writeMessage("Folder " . $folder . " retrieved", 0, "5");
+                $this->sessionStarted = true;
             } catch (Exception $e) {
                 $this->debug->writeMessage("Exception catched", 0, "EXCEPTION");
                 ob_start();var_dump($e);
@@ -75,13 +82,18 @@ class MapiSessionHandler
 
     function __destruct() {
         $this->debug->writeMessage("Starting Mapi objects destruction");
-        $this->debug->writeMessage("Unsetting", 0, "1");
-        unset($this->folder);
-        unset($this->mailbox);
-        unset($this->session);
-        unset($this->mapiProfile);
-        unset($this->profileDB);
-        $this->debug->writeMessage("Mapi objects unset done", 0, "2");
+        if ($this->sessionStarted) {
+            $this->debug->writeMessage("Unsetting", 0, "1");
+            unset($this->folder);
+            unset($this->mailbox);
+            unset($this->session);
+            unset($this->mapiProfile);
+            unset($this->profileDB);
+            $this->debug->writeMessage("Mapi objects unset done", 0, "2");
+        } else {
+            unset($this->profile);
+            unset($this->mapiDB);
+        }
     }
 
     private function openMailboxFolder($folder) {
@@ -99,6 +111,41 @@ class MapiSessionHandler
 
     public function getMailbox() {
         return $this->mailbox;
+    }
+
+    /**
+     * Call the PHP bindings to create or get a user profile
+     *
+     * @param   string  $user: user's email address
+     * @param   string  $password: user's account password
+     *
+     * @return  boolean The profile operation was successful or not
+     *
+     */
+
+    public function getProfile($user, $password) {
+        $this->debug->writeMessage("Getting or creating the profile");
+
+        $emailParts = explode('@', $user, 2);
+        $username = $emailParts[0];
+        $realm = $emailParts[1];
+        $profileName = $user;
+        $pathDB = OpenchangeConfig::$profileLocation;
+        $server = OpenchangeConfig::$openchangeServerIP;
+        $domain = OpenchangeConfig::$openchangeServerDomain;
+
+        $this->debug->writeMessage($pathDB, 1, "Parameter");
+        $this->debug->writeMessage($profileName, 1, "Parameter");
+        $this->debug->writeMessage($username, 1, "Parameter");
+        $this->debug->writeMessage(substr($password, 0, 1) . "... is secret!", 1, "Parameter");
+        $this->debug->writeMessage($domain, 1, "Parameter");
+        $this->debug->writeMessage($realm, 1, "Parameter");
+        $this->debug->writeMessage($server, 1, "Parameter");
+
+        $this->mapiDB = new MAPIProfileDB($pathDB);
+        $this->profile = $this->mapiDB->createAndGetProfile($profileName, $username, $password, $domain, $realm, $server);
+
+        return $this->profile ? true : false;
     }
 }
 ?>
