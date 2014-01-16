6250148f20773a7bfb5abf725ea9fa5743c77640
Miguel Julián <mjulian@zentyal.com>
2014-01-16 16:45:47 +0100

    Improved logging features to zentyal_oc_login plugin
     * Also some code cleaning

--- plugins/zentyal_oc_login/zentyal_oc_login.php
+++ plugins/zentyal_oc_login/zentyal_oc_login.php
@@ -1,37 +1,42 @@
 <?php
-require_once(dirname(__FILE__) . '/../zentyal_lib/OpenchangeConfig.php');
-require_once(dirname(__FILE__) . '/../zentyal_lib/MapiSessionHandler.php');
+    require_once(dirname(__FILE__) . '/../zentyal_lib/OpenchangeConfig.php');
+    require_once(dirname(__FILE__) . '/../zentyal_lib/MapiSessionHandler.php');
+    require_once(dirname(__FILE__) . '/../zentyal_lib/OpenchangeDebug.php');
+
+    /**
+     * Roundcube plugin that will handle the user Openchange profile
+     *
+     * This Roundcube plugin will check the user credentials against the
+     * Openchange server once the IMAP login has been successful.
+     * - It is triggered with the 'login_after' hook
+     * - If something happens the user will be redirected to the login page
+     *   (again) logging some information in the roundcube-openchange log.
+     *
+     * @author  Miguel Julian <mjulian@zentyal.com>
+     */
 
 class zentyal_oc_login extends rcube_plugin
 {
     public $task = 'login';
 
-    private $handle;
-
-    private function debug_msg($string)
-    {
-        if (OpenchangeConfig::$debugEnabled) {
-            fwrite($this->handle, $string);
-        }
-    }
+    private $debug;
 
     function init()
     {
-        $this->handle = fopen(OpenchangeConfig::$logLocation, 'a');
+        $this->debug = new OpenchangeDebug();
+
+        $this->debug->writeMessage("Starting the zentyal_oc_login plugin.", 0, "login");
+
         $this->rc = rcmail::get_instance();
         $this->load_config();
 
-        /* we use login after instead the authenticate hook, because, for the
-         * "first login check" we will use IMAP instead of ADirectory
-         */
-        //$this->add_hook('authenticate', array($this, 'authenticate'));
         $this->add_hook('login_after', array($this, 'checkMapiProfile'));
         //to perform a logout it's only needed a GET request to /?_task=logout
     }
 
     function checkMapiProfile($args)
     {
-        $this->debug_msg("Starting the checkMapiProfile function\n");
+        $this->debug->writeMessage("Starting the checkMapiProfile function", 0, "login");
 
         $user = get_input_value('_user', RCUBE_INPUT_POST);
         $password = get_input_value('_pass', RCUBE_INPUT_POST);
